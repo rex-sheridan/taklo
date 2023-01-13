5e4944c1cc38e79efea8f61f172d94ba2a6f0a6a
Rex Sheridan <rex.sheridan@gmail.com>
2023-01-12 21:03:49 -0500

    Fixing mystery test problem

--- test/rex_sheridan/taklo/webhooks_test.clj
+++ test/rex_sheridan/taklo/webhooks_test.clj
@@ -13,12 +13,12 @@
   (let [callback-url "callback-url"
         model-id "model-id"]
     (is (= {:headers
-            {"Authorization" "OAuth oauth_consumer_key=\"\" oauth_token=\"\""}
-            :accept :json
-            :debug false
-            :debug-body true
-            :method :post
-            :url "https://api.trello.com/1/webhooks"
-            :query-params
-            {:callbackURL callback-url :idModel model-id}}
-           (subject/create-webhook callback-url model-id)))))
+            {"Authorization" "OAuth oauth_consumer_key=\"\", oauth_token=\"\""},
+            :accept :json,
+            :debug false,
+            :debug-body true,
+            :method :post,
+            :url "https://api.trello.com/1/webhooks",
+            :query-params {:callbackURL callback-url, :idModel model-id}}
+           (subject/create-webhook callback-url model-id)
+           ))))
